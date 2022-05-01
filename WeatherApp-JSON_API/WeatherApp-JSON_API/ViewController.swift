dee437dfc87efe0a217285f2c4a15620612d6ce1
RinKusChi <meyuzak@gmail.com>
2022-05-02 00:09:48 +0300

    01 May 2022
    Dynamic Location

--- WeatherApp-JSON_API/WeatherApp-JSON_API/ViewController.swift
+++ WeatherApp-JSON_API/WeatherApp-JSON_API/ViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
     
     
     @IBOutlet weak var weatherIcon: UIImageView!
@@ -25,22 +26,38 @@ class ViewController: UIViewController {
     
     let client = DarkSkyApiClient()
     
+    var locationManager : CLLocationManager = CLLocationManager()
+    
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        client.getCurrentWeather(at: Coordinate.alcatrazIsland) { currentWeather, error in
-            
-            if let currentWeather = currentWeather {
-                let viewModel = CurrentWeatherModel(data: currentWeather)
-                
-                DispatchQueue.main.async {
-                    self.showWeather(model: viewModel)
-                }
-            }
-            
+        indicator.isHidden = true
+        
+        locationManager.requestWhenInUseAuthorization() // uygulama açıldığında konum bilgisi izni isteyecek.
+        locationManager.delegate = self
+        
+        if CLLocationManager.locationServicesEnabled() { // konum servisi cihazda açık mı?
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers // doğruluk payı - 3km bizim için yeterli
+            locationManager.startUpdatingLocation() // konum bilgisi açılmaya başlandı
         }
+    }
+    
+    
+    @IBAction func refreshButtonClicked(_ sender: Any) {
+        indicator.isHidden = false
+        indicator.startAnimating()
+        
+        locationManager(locationManager, didUpdateLocations: [])
+        
         
     }
+    
+    
 
 }
 
@@ -53,7 +70,30 @@ extension ViewController{ // Functions
         lblHumidityValue.text = model.humidity
         lblPrecipitationValue.text = model.precipitationProbability
         weatherIcon.image = model.icon
-        
+        indicator.stopAnimating()
+    }
+    
+    func updateWeather(coordinate : Coordinate) {
+        client.getCurrentWeather(at: coordinate) { currentWeather, error in
+            
+            if let currentWeather = currentWeather {
+                let viewModel = CurrentWeatherModel(data: currentWeather)
+                DispatchQueue.main.sync {
+                    self.showWeather(model: viewModel)
+                }
+            }
+        }
     }
     
 }
+
+extension ViewController : CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locationValue : CLLocationCoordinate2D = manager.location!.coordinate
+        let clientCoordinate = Coordinate(latitude: locationValue.latitude, longitude: locationValue.longitude)
+        print("Latitude : \(clientCoordinate.latitude), Longitude : \(clientCoordinate.longitude)")
+        updateWeather(coordinate: clientCoordinate)
+        
+    }
+}
+
